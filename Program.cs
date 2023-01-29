4761f15b8d768f8015935368aa48ab427403dea5
Alex6706 <kvslanz@gmail.com>
2023-01-29 20:46:53 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число для проверки четности ");
+        double a = Convert.ToInt32(Console.ReadLine());
+        if (a % 2 == 0)
+        {
+            Console.WriteLine("Да ");
+        }
+
+        else
+        {
+            Console.WriteLine("Нет ");
+        }
